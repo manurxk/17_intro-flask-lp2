083d5f336538a9fc79b07b3bf9d33a16d0565e24
Manurxk <armanuramirez16@gmail.com>
2024-08-25 13:10:10 -0400

    Agregando conexion

---
+++ conexion/Conexion.py
@@ -0,0 +1,14 @@
+import psycopg2
+class Conexion:
+
+    """Metodo contructor de tu perro
+    """
+    def __init__(self):
+        self.con = psycopg2.connect("dbname=veterinaria-db user=postgres host=localhost password=12345678")
+        
+        """getConexion
+
+            retorna la instancia de la base de datos
+        """
+    def getConexion(self):
+        return self.con 
